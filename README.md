bcf3ba94911eb159b86a1b31c8f1a4fe1c5e8651
Ryan Zhang <ryanzhang201011@gmail.com>
2024-03-09 15:15:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Perfect Circle
+Two short python scripts to get the 99.9% and 0.1% on [Draw a Perfect Circle](https://neal.fun/perfect-circle/).
+
+You need to install pyautogui
+
+
+## Note
+In both scripts, there is a variable called ```center_of_circle```. You must change it to the position of the white dot relative to your screen.
---
+++ lowestScore.py
@@ -0,0 +1,49 @@
+import math
+import pyautogui
+from time import sleep
+
+def point_rotation(x, y, degrees):
+    # Convert degrees to radians
+    angle_radians = degrees * (math.pi / 180)
+
+    # Rotation matrix
+    rotation_matrix = [
+        [math.cos(angle_radians), -math.sin(angle_radians)],
+        [math.sin(angle_radians), math.cos(angle_radians)]
+    ]
+
+    # Apply rotation
+    x_prime = rotation_matrix[0][0] * x + rotation_matrix[0][1] * y
+    y_prime = rotation_matrix[1][0] * x + rotation_matrix[1][1] * y
+
+    return x_prime, y_prime
+
+
+center_of_circle = (960, 550) # Different on other screens
+radius = 375
+start_radius = 125
+vertices = 0.1
+
+sleep(2)
+
+pyautogui.moveTo(center_of_circle[0], center_of_circle[1]+start_radius)
+pyautogui.mouseDown()
+pyautogui.moveTo(center_of_circle[0], center_of_circle[1]+radius)
+
+for i in range(int(360*vertices)):
+    
+
+    mouse_coord = (pyautogui.position().x, pyautogui.position().y)
+    rotation = 360 / (360 * vertices)
+
+    mouse_coord_prime = point_rotation(
+        mouse_coord[0]-center_of_circle[0],
+        mouse_coord[1]-center_of_circle[1],
+        rotation)
+    
+    pyautogui.moveTo(
+        mouse_coord_prime[0] + center_of_circle[0],
+        mouse_coord_prime[1] + center_of_circle[1],
+        )
+    
+pyautogui.mouseUp()
